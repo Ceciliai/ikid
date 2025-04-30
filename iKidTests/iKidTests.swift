e4c0902b39408eb0c7be8e2531ff3cd1023bab45
Ceciliai <seaoneseeyou@gmail.com>
2025-04-30 15:34:23 -0700

    Initial Commit

---
+++ iKidTests/iKidTests.swift
@@ -0,0 +1,17 @@
+//
+//  iKidTests.swift
+//  iKidTests
+//
+//  Created by Haiyi Luo on 4/30/25.
+//
+
+import Testing
+@testable import iKid
+
+struct iKidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
